8c134d408310cf392aa224b17f1e22b6f82cb819
jesusespigarcia <jesusespigarcia@gmail.com>
2022-04-04 09:19:52 +0200

    delete default endpoint

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AuthorsModule } from './authors/authors.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    AuthorsModule,
+    MongooseModule.forRoot('mongodb://localhost:27017/booksAndAuthors'),
+  ],
 })
 export class AppModule {}
