84fb283159b5d973346113a354a747cc85aa9719
jesusespigarcia <jesusespigarcia@gmail.com>
2022-04-05 21:08:54 +0200

    books starter module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthorsModule } from './authors/authors.module';
+import { BooksModule } from './books/books.module';
 
 @Module({
   imports: [
     AuthorsModule,
     MongooseModule.forRoot('mongodb://localhost:27017/booksAndAuthors'),
+    BooksModule,
   ],
 })
 export class AppModule {}
