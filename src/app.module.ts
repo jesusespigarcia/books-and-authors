c9967a7699f835145d0980bda2024e80b1a4e51d
jesusespigarcia <jesusespigarcia@gmail.com>
2022-04-08 10:16:08 +0200

    logging configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,20 @@
-import { Module } from '@nestjs/common';
+import { AuthModule } from './auth/auth.module';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthorsModule } from './authors/authors.module';
 import { BooksModule } from './books/books.module';
+import { LoggerMiddleware } from './middleware/logger.middleware';
 
 @Module({
   imports: [
+    AuthModule,
     AuthorsModule,
     MongooseModule.forRoot('mongodb://localhost:27017/booksAndAuthors'),
     BooksModule,
   ],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
